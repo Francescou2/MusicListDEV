c437e2c7de5267f93dc540466066f0bcbed63081
Francesco <francesco.leonardo.martino>
2016-05-31 12:12:46 +0200

    schermata login /1

---
+++ app/src/androidTest/java/app/martinofrancesco/sms16/ivu/di/uniba/it/musiclist/ApplicationTest.java
@@ -0,0 +1,13 @@
+package app.martinofrancesco.sms16.ivu.di.uniba.it.musiclist;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/app/martinofrancesco/sms16/ivu/di/uniba/it/musiclist/MainActivity.java
@@ -0,0 +1,19 @@
+package app.martinofrancesco.sms16.ivu.di.uniba.it.musiclist;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by francesco on 30/05/16.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
